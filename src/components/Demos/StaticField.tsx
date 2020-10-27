9b9577189af6251ee86959f058d187612a9e58d0
rektdeckard <friedtm@gmail.com>
2020-10-27 02:37:46 -0400

    StaticField: add toggleable options!
    The static can now be generated from cryptographically strong random
    bits at a much greater speed. We can choose to use this method or
    Math.random(), and we can also choose whether or not to normalize pixels
    to B&W or use their raw values.

--- src/components/Demos/StaticField.tsx
+++ src/components/Demos/StaticField.tsx
@@ -1,7 +1,13 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useState, useEffect } from "react";
+
+const MAX_ENTROPY = 2 ** 16;
+const INFLECTION_POINT = 2 ** 31;
 
 const StaticField: React.FC<{}> = () => {
   const ref = useRef<HTMLCanvasElement>(null);
+  const [running, setRunning] = useState<boolean>(true);
+  const [crypt, setCrypt] = useState<boolean>(true);
+  const [shouldNormalize, setShouldNormalize] = useState<boolean>(true);
 
   useEffect(() => {
     let isMounted = true;
@@ -17,6 +23,7 @@ const StaticField: React.FC<{}> = () => {
 
     const setup = () => {
       const { offsetHeight, offsetWidth } = canvas;
+      if (!offsetWidth) return;
       canvas.width = offsetWidth;
       canvas.height = offsetHeight;
       idata = context.createImageData(offsetWidth, offsetHeight);
@@ -26,32 +33,80 @@ const StaticField: React.FC<{}> = () => {
     window.addEventListener("resize", setup, { passive: true });
     setup();
 
-    const noise = (context: CanvasRenderingContext2D) => {
-      let len = buffer.length - 1;
-      while (len--) {
-        buffer[len] = Math.random() < 0.5 ? 0 : -1 >> 0;
-      }
-      context.putImageData(idata, 0, 0);
+    const normalize = (value: number, infl: number) => {
+      if (!shouldNormalize) return value;
+      return value < infl ? 0 : -1 >> 0;
     };
+
+    const noise = crypt
+      ? (context: CanvasRenderingContext2D) => {
+          let rnd = crypto.getRandomValues(
+            new Uint32Array(MAX_ENTROPY / Uint32Array.BYTES_PER_ELEMENT)
+          );
+          let len = buffer.length - 1;
+          while (len--) {
+            buffer[len] = normalize(rnd[len % rnd.length], INFLECTION_POINT);
+            if (!(len % MAX_ENTROPY)) crypto.getRandomValues(rnd);
+          }
+          context.putImageData(idata, 0, 0);
+        }
+      : (context: CanvasRenderingContext2D) => {
+          let len = buffer.length - 1;
+          while (len--) {
+            buffer[len] = Math.random() < 0.5 ? 0 : -1 >> 0;
+          }
+          context.putImageData(idata, 0, 0);
+        };
 
     (function loop() {
       if (!isMounted) return;
       noise(context);
-      requestAnimationFrame(loop);
+      if (running) requestAnimationFrame(loop);
     })();
 
     return () => {
       isMounted = false;
-      canvas?.remove();
       window.removeEventListener("resize", setup);
     };
-  }, [ref]);
+  }, [ref, running, crypt, shouldNormalize]);
 
   return (
-    <canvas
-      ref={ref}
-      style={{ backgroundColor: "black", width: "100%", borderRadius: 6 }}
-    />
+    <>
+      <div style={{ display: "flex", justifyContent: "space-evenly" }}>
+        <div>
+          <input
+            id="static-run"
+            type="checkbox"
+            checked={running}
+            onChange={() => setRunning((r) => !r)}
+          />
+          <label htmlFor="static-run">Run</label>
+        </div>
+        <div>
+          <input
+            id="static-crypto"
+            type="checkbox"
+            checked={crypt}
+            onChange={() => setCrypt((c) => !c)}
+          />
+          <label htmlFor="static-crypto">Cryptographic</label>
+        </div>
+        <div>
+          <input
+            id="static-normalized"
+            type="checkbox"
+            disabled={!crypt}
+            checked={!crypt || shouldNormalize}
+            onChange={() => setShouldNormalize((n) => !n)}
+          />
+          <label htmlFor="static-normalized">Normalized</label>
+        </div>
+      </div>
+      <canvas
+        ref={ref}
+        style={{ backgroundColor: "black", width: "100%", borderRadius: 6 }}
+      />
+    </>
   );
 };
 
