3945c8005dd4fbbc4e84f409ff2d6390e392e966
rektdeckard <friedtm@gmail.com>
2020-11-16 01:24:31 -0500

    Header+Detail: add icons to home links and vertically center

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,10 +2,25 @@ import React from "react";
 import { motion } from "framer-motion";
 
 import "./Header.css";
+import {
+  EnvelopeSimple,
+  GitBranch,
+  IconContext,
+  LinkedinLogo,
+} from "phosphor-react";
+import { IconContextProps } from "phosphor-react/dist/lib";
 
 const variants = {
   start: { opacity: 0 },
   end: { opacity: 1, transition: { duration: 0.5, delay: 0.2 } },
+};
+
+const iconStyle: IconContextProps = {
+  size: 24,
+  weight: "fill",
+  color: "currentColor",
+  mirrored: false,
+  style: { marginRight: 8 },
 };
 
 const Header: React.FC<{}> = () => {
@@ -16,11 +31,19 @@ const Header: React.FC<{}> = () => {
         engineer and maker of digital tools and toys. He's looking for a new
         opportunity.
       </h1>
-      <div className="links">
-        <a href="https://github.com/rektdeckard">GitHub</a>
-        <a href="https://linkedin.com/in/tobiasfried">LinkedIn</a>
-        <a href="mailto:friedtm@gmail.com">Email</a>
-      </div>
+      <IconContext.Provider value={iconStyle}>
+        <div className="links">
+          <a href="https://github.com/rektdeckard">
+            <GitBranch /> GitHub
+          </a>
+          <a href="https://linkedin.com/in/tobiasfried">
+            <LinkedinLogo /> LinkedIn
+          </a>
+          <a href="mailto:friedtm@gmail.com">
+            <EnvelopeSimple /> Email
+          </a>
+        </div>
+      </IconContext.Provider>
     </motion.header>
   );
 };
