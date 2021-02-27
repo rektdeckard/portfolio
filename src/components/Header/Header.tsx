dfea3ede536790e0191b80184ac6f4718473e1c1
rektdeckard <friedtm@gmail.com>
2021-02-27 18:00:35 -0500

    Header: update current position

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -28,8 +28,13 @@ const Header: React.FC<{}> = () => {
     <motion.header variants={variants} initial="start" animate="end">
       <h1 className="intro">
         <span className="highlight inverse">Tobias Fried</span> is a full-stack
-        engineer and maker of digital tools and toys. He's looking for a new
-        opportunity.
+        engineer and maker of digital tools and toys. He's currently at{" "}
+        <span className="highlight inverse">
+          <a href="https://qatalog.com" rel="noopener noreferrer">
+            Qatalog
+          </a>
+        </span>
+        .
       </h1>
       <IconContext.Provider value={iconStyle}>
         <div className="links">
